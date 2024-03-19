067b0003a856943b25bc74cb84711c8ce5a92845
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-03-19 16:48:36 +0000

    got tailwind css to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './assets/css/**/*.css', // Path to your CSS files
+    './templates/navbar.html.twig', // Path to your Twig template files
+  ],
   theme: {
     extend: {},
   },
