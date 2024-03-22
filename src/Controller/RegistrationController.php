a88d9d85cf0296b8f3767cb9f94d2b0627404b31
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-03-22 13:43:57 +0000

    sorting out routes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -27,20 +27,29 @@ class RegistrationController extends AbstractController
         ]);
     }
     public function login(Request $request): Response
-{
-    $form = $this->createForm(LoginFormType::class);
-    $form->handleRequest($request);
-    // get the login error if there is one
-    $error = $this->authenticationUtils->getLastAuthenticationError();
-
-    // last username entered by the user
-    $lastUsername = $this->authenticationUtils->getLastUsername();
-
-    return $this->render('registration/login.html.twig', [
-        'last_username' => $lastUsername,
-        'error'         => $error,
-    ]);
-}
+    {
+        $form = $this->createForm(LoginFormType::class);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+    
+    
+            return $this->redirectToRoute('home');  
+        }
+    
+        // Get the last authentication error (if any)
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+    
+        // Get the last username entered by the user
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+    
+        return $this->render('registration/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'loginForm'     => $form->createView(), 
+        ]);
+    }
     
     #[Route('/logout', name: 'app_logout')]
     public function logout(): void
--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -8,16 +8,17 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class LoginFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')          
-            ->add('password', PasswordType::class)
-            ->add('login', SubmitType::class)
-        ;
+            ->add('_username', TextType::class, ['label' => 'Email'])
+            ->add('_password', PasswordType::class, ['label' => 'Password'])
+            ->add('login', SubmitType::class, ['label' => 'Login']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
