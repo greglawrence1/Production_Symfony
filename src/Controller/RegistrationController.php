ead429dbfc2c731061df26c0a6e408b57cab4861
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-03-30 11:29:40 +0000

    login completely functioning

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,59 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\LoginFormType;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
 
 class RegistrationController extends AbstractController
-{   
-    private $authenticationUtils;
-
-    public function __construct(AuthenticationUtils $authenticationUtils)
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
-        $this->authenticationUtils = $authenticationUtils;
-    }
-
-    #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
-    {
-        return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
-        ]);
-    }
-    public function login(Request $request): Response
-    {
-        $form = $this->createForm(LoginFormType::class);
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-    
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $formData = $form->getData();
-    
-    
-            return $this->redirectToRoute('home');  
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('home');
         }
-    
-        // Get the last authentication error (if any)
-        $error = $this->authenticationUtils->getLastAuthenticationError();
-    
-        // Get the last username entered by the user
-        $lastUsername = $this->authenticationUtils->getLastUsername();
-    
-        return $this->render('registration/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error'         => $error,
-            'loginForm'     => $form->createView(), 
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
         ]);
     }
-    
-    #[Route('/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        
-    }
-
 }
-
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -35,7 +37,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->id;
     }
-    
+
     public function getEmail(): ?string
     {
         return $this->email;
