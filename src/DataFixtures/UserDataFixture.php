7685f8d522e926a0522658f4dd40a3180057a5c5
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-03-30 13:51:57 +0000

    now admin priveldegds are availale for certain things

--- src/DataFixtures/UserDataFixture.php
+++ src/DataFixtures/UserDataFixture.php
@@ -5,25 +5,33 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserDataFixture extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager)
     {
         $bob = new User();
         $bob->setEmail('bob@example.com');
-        $bob->setPassword('123');
+        $hashedPassword = $this->passwordHasher->hashPassword($bob, '123');
+        $bob->setPassword($hashedPassword);
         $bob->setRoles(['ROLE_ADMIN']);
         $manager->persist($bob);
 
         $alice = new User();
         $alice->setEmail('alice@example.com');
-        $alice->setPassword('123');
+        $hashedPassword = $this->passwordHasher->hashPassword($alice, '123');
+        $alice->setPassword($hashedPassword);
         $alice->setRoles(['ROLE_USER']);
         $manager->persist($alice);
 
         $manager->flush();
     }
-
-
 }
