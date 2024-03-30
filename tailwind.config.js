ca2a86ef5e31bec7115384bb27d8f1085156a46f
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-03-30 14:18:14 +0000

    added some more styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './assets/css/**/*.css', // Path to your CSS files
     './templates/navbar.html.twig',
+    './templates/registration/register.html.twig',
     './templates/product/product-card.html.twig' // Path to your Twig template files
   ],
   theme: {
